f7a8b0758dc759261cec03557a5a4d9a07e9952b
Matt Lane <lane.matthew@gmail.com>
2016-03-29 15:27:04 -0700

    update calculator

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express'),
   app = express();
 
 app.set('view engine', 'ejs');
-
-// app.use(express.static(__dirname + "/public"));  
+app.use(express.static(__dirname + "/public"));  
 
 app.get("/", function(req, res) {
   res.render("index", {op: undefined});
@@ -15,16 +14,17 @@ function MathOp(symbol, binaryOp) {
 }
 
 var opTable = {
-  "add": new MathOp("+", (a,b) => a+b),
-  "sub": new MathOp("-", (a,b) => a-b),
-  "mult": new MathOp("\xD7", (a,b) => a*b),
-  "div": new MathOp("\xF7", (a,b) => a/b)
-}
+  "add": new MathOp("+", (a,b) => a + b),
+  "sub": new MathOp("-", (a,b) => a - b),
+  "mult": new MathOp("\xD7", (a,b) => a * b),
+  "div": new MathOp("\xF7", (a,b) => a / b)
+};
 
 app.get('/:operation/:num1/:num2', function(req, res) {
-  var num1 = parseFloat(req.params.num1),
-    num2 = parseFloat(req.params.num2),
-    current = opTable[req.params.operation];
+  var num1 = parseFloat(req.params.num1);
+  var num2 = parseFloat(req.params.num2);
+  var current = opTable[req.params.operation];
+  
   res.render("index", {
     result: current.binaryOp(num1,num2),
     op: current.symbol,
